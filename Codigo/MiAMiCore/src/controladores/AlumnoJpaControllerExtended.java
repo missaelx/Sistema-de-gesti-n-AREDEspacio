f30961412c6bde6774fd1b02e8d3fef445378145
Missael <missaelxp@gmail.com>
2017-03-23 18:34:46 -0600

    Corrección en los Find (s)

--- Codigo/MiAMiCore/src/controladores/AlumnoJpaControllerExtended.java
+++ Codigo/MiAMiCore/src/controladores/AlumnoJpaControllerExtended.java
@@ -16,13 +16,13 @@ public class AlumnoJpaControllerExtended extends AlumnoJpaController{
     
     public Alumno getAlumnoFromNombre(String nombre){
         EntityManager em = getEntityManager();
-        Alumno alumno = (Alumno) em.createNamedQuery("Alumnos.findByNombre").setParameter("nombre", nombre).getSingleResult();
+        Alumno alumno = (Alumno) em.createNamedQuery("Alumno.findByNombre").setParameter("nombre", nombre).getSingleResult();
         em.close();
         return alumno;
     }
     public Alumno getAlumnoFromApellidos(String apellidos){
         EntityManager em = getEntityManager();
-        Alumno alumno = (Alumno) em.createNamedQuery("Alumnos.findByApellidos").setParameter("apellidos", apellidos).getSingleResult();
+        Alumno alumno = (Alumno) em.createNamedQuery("Alumno.findByApellidos").setParameter("apellidos", apellidos).getSingleResult();
         em.close();
         return alumno;
     }
