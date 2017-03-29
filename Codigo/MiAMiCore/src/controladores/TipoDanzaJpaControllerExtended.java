eacb6de4ac2cfe3b83528bfdf60d69e30f0ebd18
Missael <missaelxp@gmail.com>
2017-03-29 16:30:31 -0600

    búsquedas de danzas corregidas

---
+++ Codigo/MiAMiCore/src/controladores/TipoDanzaJpaControllerExtended.java
@@ -0,0 +1,25 @@
+package controladores;
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import modelo.TipoDanza;
+
+/**
+ *
+ * @author macbookpro
+ */
+public class TipoDanzaJpaControllerExtended extends TipoDanzaJpaController{
+    
+    public TipoDanzaJpaControllerExtended(EntityManagerFactory emf) {
+        super(emf);
+    }
+    
+    public List<TipoDanza> getAll(){
+        EntityManager em = getEntityManager();
+        List<TipoDanza> danzas = (List<TipoDanza>) em.createNamedQuery("TipoDanza.findAll").getResultList();
+        em.close();
+        return danzas;
+    }
+    
+}
