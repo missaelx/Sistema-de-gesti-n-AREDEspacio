eb2d7eb5155aa79d106027b69af12969621d1a98
Missael <missaelxp@gmail.com>
2017-03-30 17:42:06 -0600

    previo revision

--- Codigo/MiAMiCore/test/recursos/MaestroResourceTest.java
+++ Codigo/MiAMiCore/test/recursos/MaestroResourceTest.java
@@ -1,5 +1,6 @@
 package recursos;
 
+import controladores.MaestroJpaControllerExtended;
 import java.util.List;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -36,7 +37,7 @@ public class MaestroResourceTest {
     //@Test
     public void testEliminarMaestro() throws Exception {
         System.out.println("eliminarMaestro");
-        Maestro maestro = null;
+        Maestro maestro = test;
         MaestroResource instance = new MaestroResource();
         boolean expResult = false;
         boolean result = instance.eliminarMaestro(maestro);
@@ -45,51 +46,44 @@ public class MaestroResourceTest {
         fail("The test case is a prototype.");
     }
 
-    //@Test
+    @Test
     public void testModificarMaestro() throws Exception {
         System.out.println("modificarMaestro");
-        Maestro maestro = null;
+        MaestroJpaControllerExtended control = new MaestroJpaControllerExtended(emf);
+        
+        Maestro maestro = control.findMaestro(1);
+        maestro.setNombre("Modificado por modificarMaestroText");
         MaestroResource instance = new MaestroResource();
-        boolean expResult = false;
+        boolean expResult = true;
         boolean result = instance.modificarMaestro(maestro);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
-    //@Test
+    @Test
     public void testBuscarMaestroPorIdentificador() {
         System.out.println("buscarMaestroPorIdentificador");
-        int id = 0;
+        int id = 1;
         MaestroResource instance = new MaestroResource();
-        Maestro expResult = null;
         Maestro result = instance.buscarMaestroPorIdentificador(id);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result);
     }
 
-    //@Test
+    @Test
     public void testGetPagosDeSalario() {
         System.out.println("getPagosDeSalario");
-        int idMaestro = 0;
+        int idMaestro = 1;
         MaestroResource instance = new MaestroResource();
-        List<Pagodesalario> expResult = null;
+        
         List<Pagodesalario> result = instance.getPagosDeSalario(idMaestro);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result);
     }
 
-    //@Test
+    @Test
     public void testGetMaestros() {
         System.out.println("getMaestros");
         MaestroResource instance = new MaestroResource();
-        List<Maestro> expResult = null;
         List<Maestro> result = instance.getMaestros();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNotNull(result);
     }
     
 }
