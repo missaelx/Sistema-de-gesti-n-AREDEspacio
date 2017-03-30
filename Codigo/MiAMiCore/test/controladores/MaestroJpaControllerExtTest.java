cd331aefbf07d9a8d2e9feba3889481e3ebd57a7
RANGER\AndrésRoberto <techmeza@hotmail.com>
2017-03-30 17:42:02 -0600

    commit

--- Codigo/MiAMiCore/test/controladores/MaestroJpaControllerExtTest.java
+++ Codigo/MiAMiCore/test/controladores/MaestroJpaControllerExtTest.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import modelo.Maestro;
 import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 import recursos.MaestroResource;
 
 /**
@@ -24,7 +25,7 @@ public class MaestroJpaControllerExtTest {
     public MaestroJpaControllerExtTest(){
         emf = Persistence.createEntityManagerFactory("MiAMiCorePU");
     }
-    
+    @Test
     public void testGetAllMaestros(){
         System.out.println("Obtener todos los maestros, lista con maestros");
         MaestroJpaControllerExtended instancia = new MaestroJpaControllerExtended(emf);
@@ -33,17 +34,22 @@ public class MaestroJpaControllerExtTest {
         List<Maestro> resultado = instancia.getAllMaestros();
         assertEquals(espResultado, resultado);
     }
-    
+    @Test
     public void testGetAllMaestrosNull(){
-        System.out.println("Obtener todos los maestros, lista con nula");
+        System.out.println("Obtener todos los maestros, lista nula");
         MaestroJpaControllerExtended instancia = new MaestroJpaControllerExtended(emf);
         MaestroResource recurso = new MaestroResource();
         List<Maestro> espResultado = null;
         List<Maestro> resultado = listaDeMaestros;
         assertEquals(espResultado, resultado);
     }
-    
+    @Test
     public void testGetPorNombre(){
+        System.out.println("Obtener todos los maestros, lista por nombre");
+        MaestroJpaControllerExtended instancia = new MaestroJpaControllerExtended(emf);
+        MaestroResource recurso = new MaestroResource();
+        List<Maestro> espResultado = recurso.buscarMaestroNombre("Jose");
+        List<Maestro> resultado = instancia.getMaestroFromNombre("Jose");
         
     }
     
