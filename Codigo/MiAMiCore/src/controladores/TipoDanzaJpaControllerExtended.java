dfc48bb2b7c21d9e56628c6cb41cbdea25cf94c2
Missael <missaelxp@gmail.com>
2017-03-29 18:13:27 -0600

    tipodedanza

--- Codigo/MiAMiCore/src/controladores/TipoDanzaJpaControllerExtended.java
+++ Codigo/MiAMiCore/src/controladores/TipoDanzaJpaControllerExtended.java
@@ -1,27 +1,11 @@
 package controladores;
 
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author AndrésRoberto
- */
-public class TipoDanzaJpaControllerExtended {
-=======
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import modelo.TipoDanza;
 
-/**
- *
- * @author macbookpro
- */
+
 public class TipoDanzaJpaControllerExtended extends TipoDanzaJpaController{
     
     public TipoDanzaJpaControllerExtended(EntityManagerFactory emf) {
@@ -34,6 +18,5 @@ public class TipoDanzaJpaControllerExtended extends TipoDanzaJpaController{
         em.close();
         return danzas;
     }
->>>>>>> 5e5f189d65573d4f7647f48224037963c2d0ce7a
-    
+   
 }
