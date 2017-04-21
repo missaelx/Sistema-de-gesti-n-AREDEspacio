0ddf97937bc5ee3e2de9fdc457bbe42a1ab06ae3
Missael <missaelxp@gmail.com>
2017-04-20 22:11:01 -0500

    Correcion de imports

--- Codigo/MiAMiFX/src/miamifx/controllers/PrincipalController.java
+++ Codigo/MiAMiFX/src/miamifx/controllers/PrincipalController.java
@@ -6,23 +6,13 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 
 /**
  * FXML Controller class
