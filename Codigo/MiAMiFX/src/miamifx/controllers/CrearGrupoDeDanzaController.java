2c18fb5e9fdcb5c19d1d569c538847f4de02064d
RANGER\AndrésRoberto <techmeza@hotmail.com>
2017-05-02 17:09:57 -0500

    correcciones del repositorio ¬¬ y nueva interfaz :)

--- Codigo/MiAMiFX/src/miamifx/controllers/CrearGrupoDeDanzaController.java
+++ Codigo/MiAMiFX/src/miamifx/controllers/CrearGrupoDeDanzaController.java
@@ -1,14 +1,12 @@
 package miamifx.controllers;
 
 import com.jfoenix.controls.JFXTimePicker;
+import java.math.BigDecimal;
 import java.net.URL;
-<<<<<<< HEAD
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Date;
 import java.util.List;
-=======
->>>>>>> 7df33928cf4a2e7bac079efeff58cd0135520004
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -16,9 +14,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
 import modelo.GrupoClase;
 import modelo.Horario;
+import modelo.Maestro;
 import modelo.TipoDanza;
 import recursos.DanzaResource;
 
@@ -36,20 +38,17 @@ public class CrearGrupoDeDanzaController implements Initializable {
             entradaViernes,salidaViernes,entradaSabado,salidaSabado;
     @FXML
     private Label nombreDanza;
-<<<<<<< HEAD
+
     @FXML
     private ComboBox<Maestro> listaCBmaestros;
     @FXML
     private TextField costoMensual;
     @FXML
     private Spinner<Float> porcentajeMaestro;
-
-=======
-    
->>>>>>> 7df33928cf4a2e7bac079efeff58cd0135520004
     private TipoDanza tipoDanza;
     private GrupoClase grupoDanza;
     private AdministrarDanzasController controlador;
+    private List <Horario> horario;
 
     /**
      * Initializes the controller class.
@@ -74,33 +73,8 @@ public class CrearGrupoDeDanzaController implements Initializable {
     private void cancelar(ActionEvent evento) {
         ((Node) (evento.getSource())).getScene().getWindow().hide();
     }
-    @FXML
-<<<<<<< HEAD
-    public void horasClaseCompletas() {
-        if (entradaLunes.getValue().equals(null) || salidaLunes.getValue().equals(null)) {
-            botonGuardar.setDisable(true);
-        }
-        if (entradaMartes.getValue().equals(null) || salidaMartes.getValue().equals(null)) {
-            botonGuardar.setDisable(true);
-        }
-        if (entradaMiercoles.getValue().equals(null) || salidaMiercoles.getValue().equals(null)) {
-            botonGuardar.setDisable(true);
-        }
-        if (entradaJueves.getValue().equals(null) || salidaJueves.getValue().equals(null)) {
-            botonGuardar.setDisable(true);
-        }
-        if (entradaViernes.getValue().equals(null) || salidaViernes.getValue().equals(null)) {
-            botonGuardar.setDisable(true);
-        }
-        if (entradaSabado.getValue().equals(null) || salidaSabado.getValue().equals(null)) {
-            botonGuardar.setDisable(true);
-        }
-
-        if (!entradaLunes.getValue().equals(null) && !salidaLunes.getValue().equals(null)) {
-=======
     public void horaClaseComplta(){
         if(!entradaLunes.getValue().equals(null) && !salidaLunes.getValue().equals(null)){
->>>>>>> 7df33928cf4a2e7bac079efeff58cd0135520004
             botonGuardar.setDisable(false);
         }else{botonGuardar.setDisable(true);}
         if(!entradaMartes.getValue().equals(null) && !salidaMartes.getValue().equals(null)){
@@ -119,7 +93,6 @@ public class CrearGrupoDeDanzaController implements Initializable {
             botonGuardar.setDisable(false);
         }else{botonGuardar.setDisable(true);}
     }
-<<<<<<< HEAD
 
     public Horario setHorario(LocalTime entrada, LocalTime salida,String dia) {
         //entrada.atDate(LocalDate.now());
@@ -174,34 +147,6 @@ public class CrearGrupoDeDanzaController implements Initializable {
         recurso.crearGrupoClase(grupoDanza);
         controlador.setTabla();
         botonGuardar.getScene().getWindow().hide();
-=======
-    
-    @FXML
-    private void guardar(ActionEvent evento) {
-        try {
-            DanzaResource recurso = new DanzaResource();
-            Alert alerta = new Alert(Alert.AlertType.INFORMATION);
-            alerta.setContentText("Algun dia no tiene hora de salida, por favor ingresa la informacion completa");
-            
-            if (entradaSabado.converterProperty().toString().isEmpty()) {
-                alerta.show();
-            } else {
-                grupoDanza = new GrupoClase();
-                grupoDanza.setActivo(true);
-                grupoDanza.setIdTipoDanza(tipoDanza);
-                //grupoDanza.setNombre(tfNombreDanza.getText());
-                //grupoDanza.setDescripcion(tfDescripcion.getText());
-                recurso.crearGrupoClase(grupoDanza);
-                controlador.setTabla();
-                botonGuardar.getScene().getWindow().hide();
-
-            }
-        } catch (Exception e) {
-            Alert alerta = new Alert(Alert.AlertType.ERROR);
-            alerta.show();
-
-        }
->>>>>>> 7df33928cf4a2e7bac079efeff58cd0135520004
-
     }
+    
 }
---
+++ Codigo/MiAMiFX/src/miamifx/controllers/ListaAlumnosController.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miamifx.controllers;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import modelo.Alumno;
+import modelo.GrupoClase;
+import recursos.AlumnoResource;
+
+/**
+ * FXML Controller class
+ *
+ * @author AndrésRoberto
+ */
+public class ListaAlumnosController implements Initializable {
+    
+    @FXML
+    private ComboBox comboBusqueda;
+    @FXML
+    private TextField campoBusqueda;
+    @FXML
+    private TableView<Alumno> tablaAlumnos;
+    @FXML
+    private TableColumn columnaNombre, columnoApellidos, columnaAsistencia;
+    @FXML
+    private ImageView fotoAlumno;
+    @FXML
+    private Button bAgregar, bEliminar, bAsistencia,bCerrar;
+    private GrupoClase grupoDanza;
+    private List<Alumno> listaAlumnos;
+    private AdministrarDanzasController controlador;
+    
+    
+    
+    @FXML
+    private void buscarAlumno(ActionEvent event) {
+        AlumnoResource recurso = new AlumnoResource();
+        //ObservableList lista = FXCollections.observableArrayList();
+
+        List<Alumno> listaBusqueda = new ArrayList<>();
+
+        if (comboBusqueda.getValue().toString().equals("Nombre")) {
+            listaBusqueda = recurso.buscarAlumnoPorNombre(campoBusqueda.getText());
+        } else if (comboBusqueda.getValue().toString().equals("Correo")) {
+            listaBusqueda = recurso.buscarAlumnoPorCorreo(campoBusqueda.getText());
+        }
+
+        ObservableList lista = FXCollections.observableList(listaBusqueda);
+        tablaAlumnos.setItems(lista);
+
+    }
+    
+    @FXML
+    private void activarBusqueda(ActionEvent event) {
+        //btnBuscar.setDisable(false);
+        campoBusqueda.setDisable(false);
+    }
+    
+    public void setTablaAgregar() {
+        tablaAlumnos.refresh();
+        AlumnoResource recurso = new AlumnoResource();
+        ObservableList lista = FXCollections.observableArrayList(recurso.visualizarRegistros());
+        columnaNombre.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+        columnoApellidos.setCellValueFactory(new PropertyValueFactory<>("apellidos"));
+        columnaAsistencia.setCellValueFactory(new PropertyValueFactory<>("correo"));
+        tablaAlumnos.setItems(lista);
+
+    }
+    
+    public void setTabla() {
+        tablaAlumnos.refresh();
+        AlumnoResource recurso = new AlumnoResource();
+        //ObservableList lista
+    }
+    
+    private void activarBotones() {
+        this.bEliminar.setDisable(false);
+        this.bCerrar.setDisable(false);
+        
+    }
+    
+    public void setControlador(AdministrarDanzasController controlador) {
+        this.controlador = controlador;
+    }
+    public void setGrupoDanza(GrupoClase grupoDanza){
+        this.grupoDanza = grupoDanza;
+    }
+    @FXML
+    private void cerrar(ActionEvent evento) {
+        ((Node) (evento.getSource())).getScene().getWindow().hide();
+    }
+    
+    
+    public void onTableSelection() {
+        activarBotones();
+        Alumno alumnoSeleccionado = (Alumno) tablaAlumnos.getSelectionModel().getSelectedItem();
+        if (alumnoSeleccionado.getFoto() != "" && alumnoSeleccionado.getFoto() != null) {
+            Image image = null;
+            File file;
+            try {
+                file = new File(alumnoSeleccionado.getFoto());
+                image = new Image(file.toURI().toURL().toExternalForm());
+                fotoAlumno.setImage(image);
+            } catch (MalformedURLException ex) {
+                System.out.println("Ruta incorrecta");
+            }
+        } else {
+            Path currentRelativePath = Paths.get("");
+            String currentPath = currentRelativePath.toAbsolutePath().toString();
+            String dfu = currentPath + "/USERSPICTURES/userDefault.png";
+            File file = new File(dfu);
+            try {
+                Image image = new Image(file.toURI().toURL().toExternalForm());
+                fotoAlumno.setImage(image);
+            } catch (MalformedURLException ex1) {
+                Logger.getLogger(EditarAlumnoController.class.getName()).log(Level.SEVERE, null, ex1);
+            }
+        }
+    }
+    
+    public void agregarAlumnoALista(){
+        grupoDanza.getAlumnoList().add(tablaAlumnos.getSelectionModel().getSelectedItem());
+        
+    }
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
